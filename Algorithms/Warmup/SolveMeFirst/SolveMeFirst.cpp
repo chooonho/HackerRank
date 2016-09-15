38d0d316bc2be14f49cf55f9e3b75eb49345089f
ch <nonopanda.ch@gmail.com>
2016-09-16 00:07:10 +0800

    Created project and added first solution to warmup challenge

---
+++ Algorithms/Warmup/SolveMeFirst/SolveMeFirst.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int solveMeFirst(int a, int b)
+{
+	// Hint: Type return a+b; below
+	return a + b;
+}
+
+int main()
+{
+	int num1, num2;
+	int sum;
+
+	std::cin >> num1 >> num2;
+	sum = solveMeFirst(num1, num2);
+	std::cout << sum;
+
+	return 0;
+}
